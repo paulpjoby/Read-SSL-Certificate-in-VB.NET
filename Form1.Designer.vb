ff26e9f09a6d609111494bbae25fccf1978df8d6
SnatchDreams <snatchdreams@gmail.com>
2024-01-01 23:15:42 +0530

    Source code

---
+++ Form1.Designer.vb
@@ -0,0 +1,72 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Button1 = New Button()
+        UrlTextBox = New TextBox()
+        OutputTextBox = New TextBox()
+        SuspendLayout()
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(930, 12)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(94, 29)
+        Button1.TabIndex = 0
+        Button1.Text = "Fetch"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' UrlTextBox
+        ' 
+        UrlTextBox.Location = New Point(12, 14)
+        UrlTextBox.Name = "UrlTextBox"
+        UrlTextBox.Size = New Size(912, 27)
+        UrlTextBox.TabIndex = 1
+        ' 
+        ' OutputTextBox
+        ' 
+        OutputTextBox.Dock = DockStyle.Bottom
+        OutputTextBox.Location = New Point(0, 118)
+        OutputTextBox.Multiline = True
+        OutputTextBox.Name = "OutputTextBox"
+        OutputTextBox.Size = New Size(1036, 508)
+        OutputTextBox.TabIndex = 2
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(1036, 626)
+        Controls.Add(OutputTextBox)
+        Controls.Add(UrlTextBox)
+        Controls.Add(Button1)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents UrlTextBox As TextBox
+    Friend WithEvents OutputTextBox As TextBox
+End Class
---
+++ Form1.vb
@@ -0,0 +1,37 @@
+﻿Imports System.Net.Http
+Imports System.Net.Security
+Imports System.Security.Cryptography.X509Certificates
+Public Class Form1
+    Private Function CertificateValidationCallback(request As HttpRequestMessage, certificate As X509Certificate2, chain As X509Chain, sslPolicyErrors As SslPolicyErrors) As Boolean
+
+        For Each element In chain.ChainElements
+            Dim cert As X509Certificate2 = element.Certificate
+            Me.Invoke(Sub()
+                          OutputTextBox.Text &= "Subject: " + cert.Subject _
+                          & vbCrLf & "Issuer: " + cert.Issuer _
+                          & vbCrLf & "Effective Date: " + cert.NotBefore _
+                          & vbCrLf & "Expiry Date: " + cert.NotAfter _
+                          & vbCrLf & "Raw Content : " + cert.GetRawCertDataString _
+                          & vbCrLf & "--------------------------------------------" & vbCrLf & vbCrLf & vbCrLf
+                      End Sub)
+        Next
+
+        Return True
+    End Function
+    Async Sub MakeRequest()
+        Dim handler As HttpClientHandler = New HttpClientHandler()
+        handler.ServerCertificateCustomValidationCallback = AddressOf CertificateValidationCallback
+
+        Dim client As HttpClient = New HttpClient(handler)
+        Using response As HttpResponseMessage = Await client.GetAsync(UrlTextBox.Text)
+            If response.StatusCode = Net.HttpStatusCode.OK Then
+                ' 
+            End If
+        End Using
+    End Sub
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        OutputTextBox.Text = ""
+        Dim t1 As Task = New Task(AddressOf MakeRequest)
+        t1.Start()
+    End Sub
+End Class
